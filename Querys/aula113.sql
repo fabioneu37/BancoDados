20035389b355290c9f3d4dc3ab78c948c8f2c904
Fabio Souza <superneu@gmail.com>
2023-10-18 14:48:17 -0300

    atualizacao

--- Querys/aula113.sql
+++ Querys/aula113.sql
@@ -1,1 +1,27 @@
 use universidade_u;
+
+delimiter $$
+create procedure proc_case()
+comment 'Exemplo de uso do case'
+begin
+   
+   declare data_teste date;
+   
+   set data_teste = curdate();
+   
+   case 
+       when data_teste = curdate() then select curdate() as data_atual, data_teste, 'Presente';
+       when data_teste < curdate() then select curdate() as data_atual, data_teste, 'Passado';
+       when data_teste > curdate() then select curdate() as data_atual, data_teste, 'Futuro';
+	   else 
+            select 'Nenhuma das anteriores';
+	end case;
+end
+$$
+delimiter ;
+
+call proc_case;
+
+show procedure status where Db = 'universidade_u';
+drop procedure universidade_u.proc_case;
+
